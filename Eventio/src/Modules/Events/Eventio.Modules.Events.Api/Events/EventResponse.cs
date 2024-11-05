9c48c60064947fd02d6ca392501be8d79736c95a
Ronny Delgado <delgado.ronny@gmail.com>
2024-11-05 17:14:16 -0600

    feat: program get event use case in events module

---
+++ Eventio/src/Modules/Events/Eventio.Modules.Events.Api/Events/EventResponse.cs
@@ -0,0 +1,10 @@
+namespace Eventio.Modules.Events.Api.Events;
+
+public sealed record EventResponse(
+    Guid Id,
+    string Title,
+    string Description,
+    string Location,
+    DateTime StartsAtUtc,
+    DateTime? EndsAtUtc
+);
---
+++ Eventio/src/Modules/Events/Eventio.Modules.Events.Api/Events/GetEvent.cs
@@ -0,0 +1,31 @@
+using Eventio.Modules.Events.Api.Database;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+namespace Eventio.Modules.Events.Api.Events;
+
+public static class GetEvent
+{
+    public static void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("events/{id}", async (Guid id, EventsDbContext context) =>
+        {
+            EventResponse? @event = await context.Events
+                .Where(e => e.Id == id)
+                .Select(e => new EventResponse(
+                    e.Id,
+                    e.Title,
+                    e.Description,
+                    e.Location,
+                    e.StartsAtUtc,
+                    e.EndsAtUtc
+                ))
+                .SingleOrDefaultAsync();
+
+            return @event is null ? Results.NotFound() : Results.Ok(@event);
+
+        }).WithTags(Tags.Events);
+    }
+    
+}
